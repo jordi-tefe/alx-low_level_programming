b25182c97faafe72b4aafb72191fbb55a8ee72aa
Yordanos Teferi <jordisavage90@gmail.com>
2023-07-13 05:26:17 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /*
-  main -entry point
- 
-  return:nalways 0 (success)
+*  main -entry point
+*
+*  return:nalways 0 (success)
  */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
