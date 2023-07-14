d02d0ce4eb5b5ad81d19773695809c78f7f2be2a
Yordanos Teferi <jordisavage90@gmail.com>
2023-07-14 14:01:53 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-  int n;
+int n;
 
-  for (n = 0 ; n < 10 ; n++)
-  {
-    putchar(n + '0');
-    if (n < 9)
-    {
-      putchar(',');
-      putchar(32);
-    }
-  }
-  putchar('\n');
-  return (0);
+for (n = 0 ; n < 10 ; n++)
+{
+putchar(n + '0');
+if (n < 9)
+{
+putchar(',');
+putchar(32);
+}
+}
+putchar('\n');
+return (0);
 }
